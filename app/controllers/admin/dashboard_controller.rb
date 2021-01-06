1767487665b36e636b8d0572b0e0362735dcd202
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-06 20:00:40 +0000

    improved dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,10 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
   
   def show
+    @categories_count = Category.count
+    @products_count = Product.count
   end
+
+
+
 end
