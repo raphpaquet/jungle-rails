67e1dc9874133dc1de87fe295e81d29b75b11410
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-11 18:40:22 +0000

    added sales method

---
+++ app/models/sale.rb
@@ -0,0 +1,21 @@
+class Sale < ActiveRecord::Base
+
+
+  # AR Scope
+  def self.active
+    where('sales.starts_on <= ? AND sales.ends_on >= ?', Date.current, Date.current)
+  end 
+
+  def finished?
+    ends_on < Date.current
+  end
+  
+  def upcoming?
+    starts_on > Date.current
+  end 
+
+  def active?
+    !upcoming? && !finished?
+  end
+   
+end
