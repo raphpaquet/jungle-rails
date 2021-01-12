4cad1222db06f1f4ea596a97151da786f2edcfd7
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-12 02:35:01 +0000

    method authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  
+  validates :firstname, presence: true, length: { minimum: 3 }
+  validates :lastname, presence: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.downcase.strip)
+    if user && user.authenticate(password)
+      @user = user
+    else 
+      @user = nil
+    end 
+  end 
 end
