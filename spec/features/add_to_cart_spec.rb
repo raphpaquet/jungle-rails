bdd9078a847b722e4b94fbeb2d18fb335b38132d
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-12 22:58:00 +0000

    test cart increase number

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "When they add products to cart, number of cart item increase" do
+    # ACT
+    visit root_path
+
+    expect(page).to have_content('My Cart (0)')
+
+    # VERIFY
+    first('.actions').click_on('Add')
+    
+    # DEBUG 
+    save_screenshot
+    
+    expect(page).to have_content('My Cart (1)')
+    
+
+  end
+
+end
